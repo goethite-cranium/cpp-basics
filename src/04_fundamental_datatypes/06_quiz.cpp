c55b2631dde190a648ef4c0fa5ae5390185563c6
mario <goethite.cranium@gmail.com>
2026-08-18 19:49:50 +0200

    Completed chapter 04

--- src/04_fundamental_datatypes/06_quiz.cpp
+++ src/04_fundamental_datatypes/06_quiz.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
+#include <iomanip> // For std::setprecision
 #include <stdexcept>
 
-
-void ex1()
+double getDouble()
 {
     std::cout << "Enter a double value: ";
-    double x {};
+    double x{};
     std::cin >> x;
+    return x;
+}
 
-    std::cout << "Enter a double value: ";
-    double y {};
-    std::cin >> y;
-
-    std::cout << "Enter +, -, * or /:"
-    char c {};
+char getOperator()
+{
+    std::cout << "Enter +, -, * or /:";
+    char c{};
     std::cin >> c;
+    return c;
+}
 
-    double result {};
+void printResult(double x, double y, char c)
+{
+    double result{};
     if (c == '+')
         result = x + y;
     else if (c == '-')
@@ -26,14 +30,64 @@ void ex1()
     else if (c == '/')
         result = x / y;
     else
-        throw std::invalid_argument( "The value entered is not among +, -, * or /");
-    
-    std::cout << x << ' ' << c << ' ' << y << " is " << result;
+        throw std::invalid_argument("The value entered is not among +, -, * or /");
+
+    std::cout << x << ' ' << c << ' ' << y << " is " << result << '\n';
+}
+
+void ex1()
+{
+    double x { getDouble() };
+    double y { getDouble() };
+    char c   { getOperator() };
+
+    printResult(x, y, c);
+}
+
+
+float getTowerHeight()
+{
+    std::cout << "Enter the height of the tower in meters: ";
+    float height {};
+    std::cin >> height;
+
+    return height;
+}
+
+
+float getFallenDistance(float seconds)
+{
+    return 9.8*seconds*seconds/2;
+}
+
+void calculateBallHeight(float seconds, float height)
+{
+    float distanceFromTop { height - getFallenDistance(seconds) };
+    if (distanceFromTop <= 0)
+        std::cout << "At " << seconds << " seconds, the ball is on the ground\n";
+    else
+        std::cout << "At " << seconds << " seconds, the ball is at height " << distanceFromTop << " meters\n";
+}
+
+void ex2()
+{
+    double height { getTowerHeight() };
+
+    float a { 0.0 }, b { 1.0 }, c { 2.0 }, d { 3.0 }, e { 4.0 }, f { 5.0 };
+
+    std::cout << std::fixed << std::setprecision(2);
+    calculateBallHeight(a, height);
+    calculateBallHeight(b, height);
+    calculateBallHeight(c, height);
+    calculateBallHeight(d, height);
+    calculateBallHeight(e, height);
+    calculateBallHeight(f, height);
 }
 
 int main()
 {
     ex1();
+    ex2();
 
     return 0;
 }
